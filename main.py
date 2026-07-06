ba2cbcab2fac39ffd2e178ffb06dbad6e30031cc
xiaoqi419 <xiaoqi419@gmail.com>
2026-07-06 21:45:06 +0800

    fix: listen on port 80 to match container port mapping

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ from api import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    uvicorn.run(app, access_log=False, log_level="info")
+    uvicorn.run(app, host="0.0.0.0", port=80, access_log=False, log_level="info")
